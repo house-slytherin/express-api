a58bfe38c605b6762b517e5bae2a822f6315ede3
mrylraihan <mr.ylraihan@gmail.com>
2022-01-19 23:55:19 -0500

    resubmitting backend

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose')
 const cors = require('cors')
 
 // require route files
-const exampleRoutes = require('./app/routes/example_routes')
+const taskRoutes = require('./app/routes/task_routes')
 const userRoutes = require('./app/routes/user_routes')
 
 // require middleware
@@ -56,7 +56,7 @@ app.use(express.urlencoded({ extended: true }))
 app.use(requestLogger)
 
 // register route files
-app.use(exampleRoutes)
+app.use(taskRoutes)
 app.use(userRoutes)
 
 // register error handling middleware
